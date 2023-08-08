d9277747acbbc4f1bb819eec22f99e10324fc5e6
13Nephilimm <jekokharabadze@gmail.com>
2023-08-08 12:20:46 +0400

    s

--- src/App.js
+++ src/App.js
@@ -1,23 +1,76 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
+import images from "./components/images";
+import { shuffle } from "lodash";
 
 function App() {
+  const [cards, setCards] = useState(shuffle([...images, ...images]));
+  const [activeCards, setActiveCards] = useState([]);
+  const [foundMatches, setFoundMathces] = useState([]);
+  const [click, setClick] = useState([]);
+  const [won, setWon] = useState(false);
+
+  function flipCard(index) {
+    if (won) {
+      setWon(false);
+      setCards(shuffle([...images, ...images]));
+      setFoundMathces([]);
+      setClick(0);
+    }
+
+    if (activeCards.length === 0) {
+      setActiveCards([index]);
+    }
+
+    if (activeCards.length === 1) {
+      const firstIndex = activeCards[0];
+      const secondIndex = index;
+
+      if (cards[firstIndex] === cards[secondIndex]) {
+        setFoundMathces([...foundMatches, firstIndex, secondIndex]);
+
+        if (foundMatches.length + 2 === cards.length) {
+          setWon(true);
+        }
+      }
+
+      setActiveCards([...activeCards, index]);
+    }
+
+    if (activeCards.length === 2) {
+      setActiveCards([index]);
+    }
+
+    setClick(+click + 1);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Memory Game</h1>
+      <div className="board">
+        {cards.map((card, index) => {
+          const flippedToFront =
+            activeCards.indexOf(index) !== -1 ||
+            foundMatches.indexOf(index) !== -1;
+          return (
+            <div
+              className={"card-outer " + (flippedToFront ? "flipped" : "")}
+              onClick={() => {
+                flipCard(index);
+              }}
+            >
+              <div className="card">
+                <div className="front">
+                  <img src={card} alt="picture" />
+                </div>
+                <div className="back" />
+              </div>
+            </div>
+          );
+        })}
+      </div>
+      <h2>Clicks: {click}</h2>
+      {won && alert("You Won The Game!")}
     </div>
   );
 }
